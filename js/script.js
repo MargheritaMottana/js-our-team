667f6b9bfa090ff9de441a481fc5e907e9d06129
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-27 16:32:05 +0200

    creato array del tram con gli oggetti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,66 @@
 console.log('Hellou :3')
+
+/*
+
+1. MILESTONE 0:
+Creare l'array di oggetti con le informazioni fornite.
+
+2. MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+3. MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+DATI PER ESERCIZIO:
+
+Name            Role                    Image
+Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+*/
+
+// creo un array con gli oggetti
+const ourTeam = [
+
+    // creo gli oggetti
+    person1 = {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        pic: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    person2 = {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+
+    person3 = {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+
+    person4 = {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    person5 = {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+
+    person6 = {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+
+];
